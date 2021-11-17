0194e7f7be8d41a81d0b05536b95be2ea3ef0113
Muhammed Bayat <46926098+muhammed-bayat@users.noreply.github.com>
2021-11-17 13:28:55 +0300

    opencv ve js için ayrı ayrı camera ayarlamaları yapıldı

--- detectme/views.py
+++ detectme/views.py
@@ -5,6 +5,9 @@ from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 import cv2
 import threading
+import imutils
+
+from detectme.camera import IPWebCam ,VideoCamera_py
 
 # https://blog.miguelgrinberg.com/post/video-streaming-with-flask/page/8
 
@@ -23,7 +26,34 @@ def auto_record(request):
 
     return render(request, "auto_record.html" )
 
+####################################
 
+
+#Display the 2 videos
+def index(request):
+    return render(request, 'streamapp/home.html')
+
+#Every time you call the phone and laptop camera method gets frame
+#More info found in camera.py
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+#Method for laptop camera
+def video_feed(request):
+	return StreamingHttpResponse(gen(VideoCamera_py()),
+                    #video type
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+#Method for phone camera
+def webcam_feed(request):
+	return StreamingHttpResponse(gen(IPWebCam()),
+                    #video type
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+######################################
     
 
 class VideoCamera(object):
@@ -45,11 +75,11 @@ class VideoCamera(object):
             (self.grabbed, self.frame) = self.video.read()
 
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    def gen(camera):
+        while True:
+            frame = camera.get_frame()
+            yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @gzip.gzip_page
